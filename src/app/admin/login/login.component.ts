f98b4f88ef6734311b541c39bfc03d820169d170
Cristobal <cmartinez@queplan.cl>
2019-07-28 19:20:43 -0400

    se crea login

--- src/app/admin/login/login.component.ts
+++ src/app/admin/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Login } from '../../models/login';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  login = 'hola login';
+
+  constructor() {
+  }
+
 
   ngOnInit() {
   }
 
+  enterLogin() {
+    console.log(this.login);
+  }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductsComponent } from './admin/products/products.component';
 import { HeaderAdminComponent } from '../app/admin/header-admin/header-admin.component';
+import { LoginComponent } from './admin/login/login.component';
 
 
 const routes: Routes = [
+{path: 'admin', component: LoginComponent},
 {path: 'admin/products', component: ProductsComponent},
-{path: 'head', component: HeaderAdminComponent}
+{path: 'head', component: HeaderAdminComponent},
 ];
 
 @NgModule({
